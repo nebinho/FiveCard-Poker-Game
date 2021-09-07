b259e64bd337a51113852a6ddcecbefe4a3c9c3e
Jonas Persson <jope2007@student.miun.se>
2021-09-07 12:33:39 +0200

    Skapat Enums för korttyp och individuella korttyper - JP

---
+++ FiveCardPokerGame/Data/Cards.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveCardPokerGame.Data
+{
+    public enum Cards
+    {
+        Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Knight, Queen, King, Ace
+    }
+}
