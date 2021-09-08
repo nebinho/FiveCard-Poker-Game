e7a52b9e3e98d6159f54545e60228ecf067a8510
Jonas Persson <Jonaskwge@gmail.com>
2021-09-08 09:56:20 +0200

    GetFiveCards Method - remove from list and lower random.next

--- FiveCardPokerGame/ViewModels/GameViewModel.cs
+++ FiveCardPokerGame/ViewModels/GameViewModel.cs
@@ -16,11 +16,14 @@ namespace FiveCardPokerGame.ViewModels
             
         private void GetFiveCards()
         {
+            int tja = 52;
             for (int i = 0; i < 5; i++)
             {
-                var magicNumber = random.Next(52);
-
+                var magicNumber = random.Next(tja);
                 Hand.Add(deckOfCards.Cards[magicNumber]);
+                deckOfCards.Cards.RemoveAt(magicNumber);
+                tja--;
+                
             }
         }
 
