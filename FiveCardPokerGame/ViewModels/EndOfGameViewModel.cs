870ad0c3c056da87770b9f94c194f9343207118c
NeboT1000 <73859153+NeboT1000@users.noreply.github.com>
2021-09-23 10:01:04 +0200

    HighscoreDatabase methods working and connected to EndOfGameViewModel

--- FiveCardPokerGame/ViewModels/EndOfGameViewModel.cs
+++ FiveCardPokerGame/ViewModels/EndOfGameViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using FiveCardPokerGame.Commands;
+using FiveCardPokerGame.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,7 @@ namespace FiveCardPokerGame.ViewModels
    public class EndOfGameViewModel : BaseViewModel
     {
         public string EndScore { get; set; }
-
+        public HighScoreDb highScoreDb = new HighScoreDb();
         public ICommand PlayAgainCommand { get; set; }
 
         public ICommand GoToStartCommand { get; set; }
@@ -20,6 +21,8 @@ namespace FiveCardPokerGame.ViewModels
 
         public EndOfGameViewModel()
         {
+            highScoreDb.SetHighscore();
+            highScoreDb.GetHighscores();
             EndScore = Global.EndScore.ToString();
             PlayAgainCommand = new PlayAgainCommand(this);
             GoToStartCommand = new GoToStartCommand(this);
