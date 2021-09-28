2f251601fdc34db6b6730c22585be2129e324939
Jonas Persson <Jonaskwge@gmail.com>
2021-09-28 10:30:52 +0200

    Designed How to play view a bit better

--- FiveCardPokerGame/ViewModels/HowToPlayViewModel.cs
+++ FiveCardPokerGame/ViewModels/HowToPlayViewModel.cs
@@ -21,7 +21,17 @@ namespace FiveCardPokerGame.ViewModels
         {
             "Pair = 5 points", "Two Pairs = 10 Points", "Three of a kind = 15 Points", "Straight = 20 Points", "Flush = 25 Points", "Full House = 30 Points", "Four of a kind = 35 Points", "Straight flush = 40 Points", "Royal Straight Flush = 50 Points!"
         };
-        public string HowToPlay { get; set; } = "You play the game by first selecting what difficulty you want to play (Number of Draws). You also have to select which player you want to play as by selecting a existing player or creating a new one. Then you press 'Start Game'. When the game has started you have as many draws as you chose on the first page to get as many points as possible and land yourself on the leaderboard! You play by getting 5 random cards from the deck. You then drag the ones you want to change to the 'Throw here zone' and then click on the red card with the writing 'Draw Card' on it to get new cards!";
         
+        public ObservableCollection<string> HowToPlay { get; set; } = new ObservableCollection<string>
+        {
+            "You play the game by first selecting what difficulty you want to play (Number of Draws).",
+            "You also have to select which player you want to play as by selecting a existing player or creating a new one.",
+            "Then you press 'Start Game'.",
+            "When the game starts your difficulty is determed by your choice on the starting page",
+            "You play by getting 5 random cards from the deck",
+            "You then drag the ones you want to change to the 'Throw here zone'", 
+            "To draw new cards you click on the red card with the writing 'Draw Card' and you will be delt new cards.",
+            "The point of the game is to get as good of a hand as possible."
+        };
     }
 }
