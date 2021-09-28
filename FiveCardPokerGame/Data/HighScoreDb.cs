a06e24586cadb775af3a38e70fe4374dbee4e727
NeboT1000 <73859153+NeboT1000@users.noreply.github.com>
2021-09-28 14:09:20 +0200

    commiting something else

--- FiveCardPokerGame/Data/HighScoreDb.cs
+++ FiveCardPokerGame/Data/HighScoreDb.cs
@@ -91,7 +91,6 @@ namespace FiveCardPokerGame.Data
                     
                 }
                 return EndOfGameViewModel.HighscoreList;
-                return EndOfGameViewModel.HighscoreList;
                 
             }
             catch (PostgresException ex)
